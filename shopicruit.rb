91a3d469fa26ae6c0514f993bddf0cbdb5b5c4a1
Bruno Paulino <brunojppb@gmail.com>
2016-08-19 01:06:08 -0300

    First commit

---
+++ shopicruit.rb
@@ -0,0 +1,19 @@
+require 'net/http'
+require 'json'
+API_ENDPOINT = "http://shopicruit.myshopify.com/products.json?page="
+current_page = 1
+products = []
+loop do
+  uri = URI("#{API_ENDPOINT}#{current_page}")
+  response = Net::HTTP.get(uri)
+  json = JSON.parse(response)
+  break unless json.has_key?('products') && !json['products'].empty?
+  current_page += 1
+  products.concat json['products']
+end
+watchs_and_clocks = products.select do |p|
+  p['product_type'].eql?('Watch') || p['product_type'].eql?('Clock')
+end
+
+price = watchs_and_clocks.flat_map { |w| w['variants'] }.map { |w| w['price'].to_f }.reduce(:+)
+puts "Total Price: $#{price.round(2)}"
